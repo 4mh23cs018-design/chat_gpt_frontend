889f051d1a8d47cea1af6e48a018f994d50b157d
4mh23cs018-design <4mh23cs018@gmail.com>
2026-02-11 12:50:03 +0530

    login page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Link, Links } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home'
+import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
-  return (  
+  return (
     <Router>
-      <Header />
+      <Header/>
       
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer />
+      <Footer/>
     </Router>
   )
 }
